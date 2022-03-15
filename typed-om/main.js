a4225a338fcdf71c54de968a308683af1fe83dad
kusmiczkibalazs <63264124+kusmiczkibalazs@users.noreply.github.com>
2022-03-15 22:41:07 +0100

    Added typeof to console log

--- typed-om/main.js
+++ typed-om/main.js
@@ -2,13 +2,13 @@ const container = document.querySelector('.container');
 
 // CSSOM
 /* let fontSize = getComputedStyle(container).fontSize.slice(0,-2);
-console.log(fontSize); // string
+console.log(fontSize, typeof fontSize); // string
 fontSize = parseInt(fontSize) + 10;
 container.style.fontSize = fontSize + 'px'; */
 
 
 // Typed OM
 let fontSize = container.computedStyleMap().get('font-size');
-console.log(fontSize); // object
+console.log(fontSize, typeof fontSize); // object
 fontSize.value += 10;
 container.attributeStyleMap.set('font-size', fontSize);
